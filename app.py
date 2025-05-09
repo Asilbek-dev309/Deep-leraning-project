b765d49f2bf94f48552657344a2e98a6a1c87e16
Asilbek-dev309 <asilbekjon0114@gmail.com>
2025-05-09 15:45:55 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+
+st.title('Transport kalssifikatsiya qilivchi model')
+
+file=st.file_uploader("Rasm yuklash",type=['png','jpeg','gif','svg'])
+if file:
+  st.image(file)
+  img=PILImage.create(file)
+  model=load_learner('transport_model.pkl')
+  prediction,pred_id,probs=model.predict(img)
+  st.success(f'Bashorat:{prediction}')
+  st.info(f'Ehtimollik:{probs[pred_id]*100:.1f}%')
+
+  #plotting
+  fig=px.bar(x=probs*100,y=model.dls.vocab)
+  st.plotly_chart(fig)
